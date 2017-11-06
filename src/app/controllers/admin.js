158a8376d8312566b798dbc988b21cfd1f4e3aec
Niclas Horstad <niclas.horstad@gmail.com>
2017-11-07 00:22:41 +0100

    Specializations can be removed now

--- src/app/controllers/admin.js
+++ src/app/controllers/admin.js
@@ -2,9 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   specializations: [],
+  cand_selected: false,
   actions: {
     addSpecialization() {
       this.get('specializations').pushObject({});
+    },
+    removeSpecialization(index) {
+      console.log(index);
+      let sp = this.get('specializations');
+      console.log(sp);
+      this.get('specializations').removeAt(index, 1);
+    },
+    cand_select() {
+      this.set('cand_selected', true);
+      return true;
+    },
+    cand_deselect() {
+      this.set('cand_selected', false);
+      return true;
     }
   }
 });
