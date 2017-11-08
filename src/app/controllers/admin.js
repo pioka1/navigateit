fe9b218dc3f34b4526ecd7b492dd61a2a4f42d1a
Niclas Horstad <niclas.horstad@gmail.com>
2017-11-08 11:44:37 +0100

    Skills tags can be added and removed from form.

--- src/app/controllers/admin.js
+++ src/app/controllers/admin.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   specializations: [],
   cand_selected: false,
+  skills_available: ["IT Project Management", "IT Strategy", "Software Development", "Organizational Studies", "Business Economics", "IT Law", "Innovation and Entrepreneurship", "Software Engineering", "Data Analytics", "Human Computer Interaction (HCI)", "Communications", "Digital Design", "Algorithms and Mathematics", "Artificial Intelligence", "IT Security", "Computer Graphics", "Cognitive Computing", "Communications", "IT Change Management", "User Experience Design (UX)", "Statistics", "Business Intelligence", "Information Systems", "Databases", "Search Engine Optimization (SEO)", "Digital Marketing", "Servers and Hardware"],
+  skills_selected: [],
   actions: {
     addSpecialization() {
       this.get('specializations').pushObject({});
@@ -13,13 +15,21 @@ export default Ember.Controller.extend({
       console.log(sp);
       this.get('specializations').removeAt(index, 1);
     },
-    cand_select() {
+    candSelect() {
       this.set('cand_selected', true);
       return true;
     },
-    cand_deselect() {
+    candDeselect() {
       this.set('cand_selected', false);
       return true;
+    },
+    addSkill(skill) {
+      this.get('skills_selected').pushObject(skill);
+      this.get('skills_available').removeObject(skill);
+    },
+    removeSkill(skill) {
+      this.get('skills_selected').removeObject(skill);
+      this.get('skills_available').pushObject(skill);
     }
   }
 });
