69abc9383c9dae867b3758caabfe616ae6b3b3f4
Niclas Horstad <niclas.horstad@gmail.com>
2017-11-17 18:47:16 +0100

    Corrected model for bulletin

--- src/app/routes/bulletin.js
+++ src/app/routes/bulletin.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('education', params.bulletin_id);
+    return this.get('store').findRecord('bulletin', params.bulletin_id);
   }
 });
